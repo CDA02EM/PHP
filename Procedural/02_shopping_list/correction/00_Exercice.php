17154c6484b4a8786636a0fd22039e113b12c2cb
mathieuCstn <mathieuconstantin01@gmail.com>
2024-01-10 10:28:14 +0100

    02_shopping_list correction

--- Procedural/02_shopping_list/correction/00_Exercice.php
+++ Procedural/02_shopping_list/correction/00_Exercice.php
@@ -12,29 +12,29 @@ function dump(array $value): void
 
 $courses = [
 	"fruits-et-legumes" => [
-        'Pommes', 
-        'Carottes', 
-        'Fraises', 
-        'Radis', 
-        'Melons', 
-        'Tomates'
+                'Pommes', 
+                'Carottes', 
+                'Fraises', 
+                'Radis', 
+                'Melons', 
+                'Tomates'
 	],
 	"epicerie" => [
-        'Pâtes', 
-        'Sauce Roquefort', 
-        'Vinaigre', 
-        'Huile', 
-        'Sucre'
+                'Pâtes', 
+                'Sauce Roquefort', 
+                'Vinaigre', 
+                'Huile', 
+                'Sucre'
 	],
 	"frais" => [
-        'Yaourt aux fruits', 
-        'Crème déssert', 
-        'Beurre'
+                'Yaourt aux fruits', 
+                'Crème déssert', 
+                'Beurre'
 	],
 	"petit-dejeuner" => [
-        'Gateaux', 
-        'Nuttela', 
-        'Céréales'
+                'Gateaux', 
+                'Nuttela', 
+                'Céréales'
 	],
 ];
 dump($courses);
@@ -53,7 +53,44 @@ function deleteFromCourseList(array &$shoppingList, string $shelf, string $produ
         $shoppingList[$shelf] = array_values($shoppingList[$shelf]);
 }
 
+function addNewShelf(array &$shoppingList, string $shelfName): void
+{
+        if(!isset($shoppingList[$shelfName])) {
+                $shoppingList[$shelfName] = [];
+        }
+}
+
+function addItem(array &$shoppingList, string $shelf, string $item): void
+{
+        if(!in_array($item, $shoppingList[$shelf])) {
+                $shoppingList[$shelf][] = $item;
+        }
+}
+
+function shoppingListInfo(array $shoppingList): array
+{
+        $totalFood = 0;
+        $shelfsInfo = [];
+
+        foreach($shoppingList as $shelfName => $shelf) {
+                $countItems = count($shelf);
+                $totalFood += $countItems;
+                $shelfsInfo[$shelfName] = $countItems;
+        }
+
+        return [
+                'totalFood' => $totalFood,
+                'totalShelf' => count($shoppingList),
+                'shelfsInfo' => $shelfsInfo,
+        ];
+}
+
 deleteFromCourseList($courses, 'fruits-et-legumes', 'Melons');
 sortShoppingList($courses);
 
+addNewShelf($courses, 'fruits-de-mer');
+addItem($courses, 'fruits-de-mer', 'calamar');
+addItem($courses, 'fruits-de-mer', 'calamar');
+
 dump($courses);
+dump(shoppingListInfo($courses));
